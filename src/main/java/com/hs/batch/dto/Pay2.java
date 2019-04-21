df9c53537267b8a6db3796f243ce9186bc34ef3c
haeseoky <haeseoky@gmail.com>
2019-04-21 16:35:01 +0900

    4

---
+++ src/main/java/com/hs/batch/dto/Pay2.java
@@ -0,0 +1,35 @@
+package com.hs.batch.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+public class Pay2 {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+
+    private Long id;
+    private Long amount;
+    private String txName;
+    private LocalDateTime txDateTime;
+
+    public Pay2(Long amount, String txName, String txDateTime) {
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = LocalDateTime.parse(txDateTime, FORMATTER);
+    }
+
+    public Pay2(Long id, Long amount, String txName, String txDateTime) {
+        this.id = id;
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = LocalDateTime.parse(txDateTime, FORMATTER);
+    }
+}
